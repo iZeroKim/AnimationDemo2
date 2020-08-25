97b2c3f426e032e8983ba6edab57cf7c21bf07df
iZeroKim <Kimgichuru6@gmail.com>
2020-08-25 12:51:41 +0300

    Added code to play ou animation

--- app/src/main/java/com/sriyank/animationdemo2/MainActivity.kt
+++ app/src/main/java/com/sriyank/animationdemo2/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.sriyank.animationdemo2
 
+import android.graphics.drawable.AnimatedVectorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //play animation
+        avdImage.setOnClickListener {
+            checkToClose()
+        }
+    }
+
+    private fun checkToClose() {
+        avdImage.setImageResource(R.drawable.avd_anim)
+        val avdAnimation = avdImage.drawable as AnimatedVectorDrawable
+        avdAnimation.start()
     }
 }
